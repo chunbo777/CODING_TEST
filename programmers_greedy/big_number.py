f8eca7f926267a6f97bfb72a042ab5d4d16aa992
서지원 <seojiwon@seojiwons-MacBook-Air.local>
2021-11-08 15:17:30 +0900

    completed

--- programmers_greedy/big_number.py
+++ programmers_greedy/big_number.py
@@ -1,25 +1,25 @@
    
-number = "33443322"
-k=4
+number = "1231234"
+k=3
 
 
 
 #못풀었음
-def solution(number, k):
-    real_k = k
-    k_list = list(number)
-    while real_k != 0:
-        for i, num in enumerate(k_list): 
-            if i == len(k_list)-1:
-                k_list = k_list[:-1]
-                real_k-=1
-                break
-            elif num < k_list[i+1]:
-                k_list.remove(num)
-                real_k-=1
-                break
+# def solution(number, k):
+#     real_k = k
+#     k_list = list(number)
+#     while real_k != 0:
+#         for i, num in enumerate(k_list): 
+#             if i == len(k_list)-1:
+#                 k_list = k_list[:-1]
+#                 real_k-=1
+#                 break
+#             elif num < k_list[i+1]:
+#                 k_list.remove(num)
+#                 real_k-=1
+#                 break
             
-    return "".join(k_list)
+#     return "".join(k_list)
 
 
 
@@ -40,6 +40,23 @@ def solution(number, k):
             #     real_k-=1
             #     break
             
-    return "".join(k_list)
+    # return "".join(k_list)
+
+#스택으로 풀이 
+def solution(number, k):
+    stack = []
+    number =  list(number)
+    stack.append(number[0])
+    # k_list = list(number)
+    for i in number[1:]:
+        while len(stack)>0 and  k > 0 and stack[-1] < i:
+            stack.pop()
+            k-=1    
+        stack.append(i)
+    # k = k - (len(number) - len(stack))
+    while k != 0:
+        stack.pop()
+        k-=1
+    return "".join(stack)
 solution(number,k)
                 
